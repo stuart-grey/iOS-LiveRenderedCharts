efdd0edee9157d041d6723d125bc9e6dda7017ed
Sam Davies <sdavies@shinobicontrols.com>
2014-08-27 18:10:40 -0600

    Initial commit of live rendering charts prototype project

---
+++ RenderedCharts/RenderedCharts.h
@@ -0,0 +1,19 @@
+//
+//  RenderedCharts.h
+//  RenderedCharts
+//
+//  Created by Sam Davies on 27/08/2014.
+//  Copyright (c) 2014 ShinobiControls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RenderedCharts.
+FOUNDATION_EXPORT double RenderedChartsVersionNumber;
+
+//! Project version string for RenderedCharts.
+FOUNDATION_EXPORT const unsigned char RenderedChartsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RenderedCharts/PublicHeader.h>
+
+
