5ef5815c047e1643b8e8c967daf13c6610413762
Sam Davies <sdavies@shinobicontrols.com>
2014-08-27 20:33:35 -0600

    Bridging header

---
+++ LiveRenderedCharts/LiveRenderedCharts-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  LiveRenderedCharts-Bridging-Header.h
+//  LiveRenderedCharts
+//
+//  Created by Sam Davies on 27/08/2014.
+//  Copyright (c) 2014 ShinobiControls. All rights reserved.
+//
+
+#ifndef LiveRenderedCharts_LiveRenderedCharts_Bridging_Header_h
+#define LiveRenderedCharts_LiveRenderedCharts_Bridging_Header_h
+
+#import <ShinobiCharts/ShinobiChart.h>
+
+#endif
